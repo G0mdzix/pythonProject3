535d7a8cda725aef2839a9cbf4fd9998bd6029dd
G0mdzix <104032431+G0mdzix@users.noreply.github.com>
2022-06-21 16:22:44 +0200

    Add files via upload
    Nowe wykresy

--- main.py
+++ main.py
@@ -15,9 +15,15 @@ N = int(Tsim / Tp)
 u_0 = 1.44
 h_target = 26
 
-k_p = 20
-T_i = 1.5
-T_d = 2.5
+P_s = 0.15
+k = 0.9
+m = 1.2
+c = 1.005
+L = 0.006 #m w mm 6
+
+k_p = 20 #
+T_i = 5.5 #
+T_d = 5.5 #
 
 h_list = []
 
@@ -41,8 +47,10 @@ e_list = [e_0]
 u_n_list = [u_0]
 
 def get_Qd(u_t):
-    for i in range(N, 287):
-        return Tp * A * B * u_t * e_list[i]
+   return 0.005 * u_t
+
+   # for i in range(N, 287):
+    #    return Tp * A * B * u_t * e_list[i]
 
 def calc_res(n):
     regulation_error_sum = 0
@@ -53,19 +61,21 @@ def calc_res(n):
 
 
 def H_loop(h0,u_n):
-    h_list.append(h0)
+   # h_list.append(h0)
     u_t = u_0
     for i in range(N):
         e_list.append((h_target - float(h_list[i])))
         u_n = k_p * (e_list[i] + (Tp / T_i) * calc_res(i) + (T_d / Tp) * (e_list[i] - e_list[i - 1]))
         # Wzor nagrzewnicy: Q = V·ρ·cp·ΔT [kW] Q = T_p*A*B*u_n*e_list[i]
        # Qd = 0.005 * u_n_list[-1]
-        temp = Tp * A * B * u_n * e_list[i]
+       # temp = Tp * A * B * u_n * e_list[i]
+        temp = ((-P_s * k * (e_list[i]/L) + get_Qd(u_t))/ m * c) - float(h_list[i - 1])
+       # temp = Tp / A * (get_Qd(u_t) - B * sqrt(h_list[0][-1][0])) + h_list[0][-1][0]
         #temp = Tp / A * (get_Qd(u_t) - B * sqrt(float(h_list[-1]))) + float(h_list[-1])
         if temp < 0:  #0
             h_list.append(25)
         elif temp > 5:  #5
-            h_list.append(27)
+            h_list.append(26)
         else:
             h_list.append(temp)
         u_t = u_t+u_n/N
@@ -92,3 +102,6 @@ def main():
 
 
 main()
+
+
+#  temp = Tp / A * (get_Qd(u_t) - B * sqrt(h_list[0][-1][0])) + h_list[0][-1][0]
